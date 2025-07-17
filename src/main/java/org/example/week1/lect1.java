9a54655349918f3aa9b052eb2f2087fef52cc6db
ehy0419 <2hy0419@naver.com>
2025-07-17 11:34:40 +0900

    첫번째 커밋

---
+++ src/main/java/org/example/week1/lect1.java
@@ -0,0 +1,21 @@
+package org.example.week1;
+
+public class lect1 {
+
+    // 학습 키워드
+    // - [ ]  JDK - Java 프로그램을 개발할 때 필요한 개발 도구 모음
+    //    - [ ]  Javac(Java Compiler) - 자바코드를 바이트코드로 변환하는 역할
+    //    - [ ]  JVM - Java 프로그램을 실행하는 가상 머신
+    //    - [ ]  바이트코드(.class) - Javac 가 변환한 마법의파일
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("\uD83C\uDF89세상을 바꾸다\uD83C\uDF89");
+        // sout 문자열 입력
+    }
+
+    // 자바 슬로건 : 한번 작성하면 어디서든 실행할 수 있도록
+    // Write Once, Run Anywhere!
+
+    // 자바 특징 중 객체지향 : 자바의 모든 것들은 객체(class)로 구성.
+}
